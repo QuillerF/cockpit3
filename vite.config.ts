35f303f0f96074378efdb6309312ec21ced2159a
yuanxiongfeng <yuanxiongfeng@persagy.com>
2023-07-21 10:35:55 +0800

    chore: 项目初始化

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import path from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  // 路径别名
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    }
+  },
+  plugins: [vue()]
+})
